8497f620f9b4b06e3fee708fc44e3199e4606b1a
Markéta Kubíková <kubikovamarket@gmail.com>
2024-08-02 23:45:58 +0200

    project initial settings

---
+++ src/main/java/com/bookwormshop/product/service/BookwormshopProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookwormshop.product.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwormshopProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwormshopProductServiceApplication.class, args);
+	}
+
+}
